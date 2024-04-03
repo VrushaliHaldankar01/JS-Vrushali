5513216e0a4f76460715f8dc1d9feed72df23e4b
VrushaliHaldankar01 <107743756+VrushaliHaldankar01@users.noreply.github.com>
2024-04-03 16:13:31 +0000

    changes saved

---
+++ basics/01_varibles.js
@@ -0,0 +1,12 @@
+const accountId=14566
+let accountEmail="vru@gmail.com"
+var accountPassword="568"
+accountCity="Jaipur"
+
+// accountId=2 //not allowed
+accountEmail="hhh@gmail.com"
+accountPassword="567"
+accountCity="goa"
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity])
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,3 @@
+"use strict"; // treat all js code as newer version
+
+// alert(5+8) //we are using node js not browser
