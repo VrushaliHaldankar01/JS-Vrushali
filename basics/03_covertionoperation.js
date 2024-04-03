8d657d433d608b932722f6127e847380cecaed4b
VrushaliHaldankar01 <107743756+VrushaliHaldankar01@users.noreply.github.com>
2024-04-03 18:13:02 +0000

    conversion of datatypes

---
+++ basics/03_covertionoperation.js
@@ -0,0 +1,18 @@
+let  score=null
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueinNumber=Number(score)
+console.log(typeof(valueinNumber))
+
+//"33" => 33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+let isloggedin=true
+let booleanisloggedin=Boolean(isloggedin)
+console.log(booleanisloggedin)
+
+//1 =>true;0 =>false
+//"" =>false
+//"vvvv"=>true
